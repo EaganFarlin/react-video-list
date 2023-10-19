43b3f583b9f5869e8604a7f736f9d2fab44e5749
6cpsmidnight <cyberelectro101@gmail.com>
2023-10-19 00:51:14 -0400

    Intermedium update, ignore this

--- src/App.jsx
+++ src/App.jsx
@@ -1,104 +1,148 @@
 import { useState } from "react";
 import "./App.css";
 
-const Sorter = () => {};
-
-const Filter = ({ videoList, onFilter }) => {
-  console.log(videoList);
-  console.log(onFilter);
-
+const Filter = ({ onFilter }) => {
   return (
     <div className="filter">
-      <button onClick={onFilter}>
-        {videoList.map((video) => {
-          return video.title + ", ";
-        })}
-      </button>
+      <p>Filter:</p>
+      <select onChange={onFilter}>
+        <option value="alphabetical">Alphabetical</option>
+        <option value="views">Views</option>
+        <option value="likes">Likes</option>
+      </select>
     </div>
   );
 };
 
-const LikeButton = ({ video }) => {
-  const [numOfLikes, setNumOfLikes] = useState(video.likes);
-  const [isLiked, setIsLiked] = useState(false);
+const SearchInput = ({ videoList, setVideoList, onChange }) => {
+  return <input type="text" onChange={onChange} />;
+};
 
-  function handleLikeButtonClick() {
-    if (isLiked === true) {
-      setNumOfLikes(numOfLikes - 1);
-    } else {
-      setNumOfLikes(numOfLikes + 1);
-    }
-    setIsLiked(!isLiked);
-  }
-
+const LikeButton = ({ video, onLike }) => {
   return (
     <div>
-      <button className={"liked-" + isLiked} onClick={handleLikeButtonClick}>
+      <button className={"liked-" + video.isLiked} onClick={onLike}>
         👍
       </button>
-      <p>{numOfLikes + " likes"}</p>
+      <p>{video.likes + " likes"}</p>
     </div>
   );
 };
 
-const Video = ({ video }) => {
+const Video = ({ videoList, setVideoList, video }) => {
+  const handleLikeButtonClick = () => {
+    video.isLiked = !video.isLiked;
+    if (video.isLiked === true) {
+      video.likes++;
+    } else {
+      video.likes--;
+    }
+
+    // If current filter is set to sort by likes, update Video List
+    if (filteredByLikes) {
+      setVideoList(videoList.sort((a, b) => b.likes - a.likes));
+    }
+
+    setVideoList(videoList.map((item) => item));
+  };
+
   return (
     <div className="video">
       <p>{video.title}</p>
       <p>{video.views} views</p>
-      <LikeButton video={video} />
+      <LikeButton
+        videoList={videoList}
+        video={video}
+        onLike={handleLikeButtonClick}
+      />
     </div>
   );
 };
 
-let videoindex = 0;
-
-function uuidv4() {
-  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
-    (
-      c ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-    ).toString(16)
+const VideoList = ({ videoList, setVideoList }) => {
+  return (
+    <div className="video-list">
+      {videoList.map((video) => (
+        <Video
+          key={video.id}
+          videoList={videoList}
+          setVideoList={setVideoList}
+          video={video}
+        />
+      ))}
+    </div>
   );
-}
+};
 
-export default function VideoList() {
+var filteredByLikes = false;
+
+export default function FilterableVideoList() {
   class VideoClass {
     constructor(title, numofViews, numOfLikes) {
       this.title = title;
       this.id = uuidv4();
       this.views = numofViews;
       this.likes = numOfLikes;
+      this.isLiked = false;
     }
   }
 
-  let video1 = new VideoClass("Top 10 Pokemon Catches of All Time", 48, 5);
+  function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+      (
+        c ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+      ).toString(16)
+    );
+  }
+
+  let video1 = new VideoClass("Top 10 Pokemon Catches of All Time", 48, 12);
   let video2 = new VideoClass(
-    "Try Not To Laugh Challenge (WENT WRONG!)",
+    "Try Not To Laugh Challenge (GONE WRONG!)",
     623_523,
     623_523
   );
-  let video3 = new VideoClass("APause Challenge on Family", 500, 32);
+  let video3 = new VideoClass("Pause Challenge on Family", 500, 12);
 
   const [videoList, setVideoList] = useState([video1, video2, video3]);
 
-  function sortByAlphabeticOrder() {
-    console.log("TEST");
-    setVideoList(videoList.sort((a, b) => a.title.localeCompare(b.title)));
-    console.log(videoList);
-  }
+  const filter = (event) => {
+    switch (event.target.value) {
+      case "alphabetical":
+        setVideoList(videoList.sort((a, b) => a.title.localeCompare(b.title)));
+        break;
+      case "views":
+        setVideoList(videoList.sort((a, b) => b.views - a.views));
+        break;
+      case "likes":
+        setVideoList(videoList.sort((a, b) => b.likes - a.likes));
+        break;
+    }
+    filteredByLikes = event.target.value === "likes" ? true : false;
+
+    setVideoList(videoList.map((item) => item));
+  };
+
+  const [searchText, setSearchText] = useState("");
+  const [visibleVideoList, setVisibleVideoList] = useState([]);
+  // const foundVideos = filterVideos(videos, searchText);
+
+  const search = (event) => {
+    console.log(event.target.value);
+    // setSearchText(newText);
+  };
 
   return (
-    <div className="videoList">
-      <Filter
+    <div className="filterable-video-list">
+      <Filter onFilter={filter}></Filter>
+      <br />
+      <SearchInput
         videoList={videoList}
-        onFilter={() => sortByAlphabeticOrder()}
-      ></Filter>
-      <div className="video-list">
-        {videoList.map((video) => (
-          <Video key={video.id} video={video} videoList={videoList} />
-        ))}
-      </div>
+        setVideoList={setVideoList}
+        onChange={search}
+      />
+      <br />
+      <VideoList videoList={videoList} setVideoList={setVideoList}></VideoList>
     </div>
   );
 }
